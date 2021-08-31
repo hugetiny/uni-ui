7ee6b81a3ba17f48db4fd9966ae6f8b15d3e470d
Neginegi <hugetiny@hotmail.com>
2021-08-31 15:16:47 +0800

    添加自定义各个按钮的颜色功能
    如题

--- uni_modules/uni-data-checkbox/components/uni-data-checkbox/uni-data-checkbox.vue
+++ uni_modules/uni-data-checkbox/components/uni-data-checkbox/uni-data-checkbox.vue
@@ -51,7 +51,7 @@
 	 * @value tag 		标签模式
 	 * @property {Boolean} multiple = [true|false] 是否多选
 	 * @property {Array|String|Number} value 默认值
-	 * @property {Array} localdata 本地数据 ，格式 [{text:'',value:''}]
+	 * @property {Array} localdata 本地数据 ，格式 [{text:'',value:'',color:''}]
 	 * @property {Number|String} min 最小选择个数 ，multiple为true时生效
 	 * @property {Number|String} max 最大选择个数 ，multiple为true时生效
 	 * @property {Boolean} wrap 是否换行显示
@@ -67,7 +67,7 @@
 
 	// import clientdb from './clientdb.js'
 	export default {
-		name: 'uniDataChecklist',
+		// name: 'uniDataChecklist',
 		// mixins: [clientdb],
 		mixins: [uniCloud.mixinDatacom || {}],
 		// model: {
@@ -141,7 +141,8 @@
 				default(){
 					return {
 						text:'text',
-						value:'value'
+						value:'value',
+						color:'#007aff'
 					}
 				}
 			}
@@ -255,6 +256,7 @@
 				return parent;
 			},
 			chagne(e) {
+			  console.log(e)
 				const values = e.detail.value
 
 				let detail = {
@@ -363,7 +365,7 @@
 			 */
 			setStyles(item, index) {
 				//  设置自定义样式
-				item.styleBackgroud = this.setStyleBackgroud(item)
+				item.styleBackgroud = this.setStyleBackgroud(item,index)
 				item.styleIcon = this.setStyleIcon(item)
 				item.styleIconText = this.setStyleIconText(item)
 				item.styleRightIcon = this.setStyleRightIcon(item)
@@ -389,12 +391,12 @@
 			 */
 			setStyleBackgroud(item) {
 				let styles = {}
-				let selectedColor = this.selectedColor?this.selectedColor:'#007aff'
+				let selectedColor = this.selectedColor?this.selectedColor:item.color?item.color:'#007aff'
 				if (this.mode !== 'list') {
 					styles['border-color'] = item.selected?selectedColor:'#DCDFE6'
 				}
 				if (this.mode === 'tag') {
-					styles['background-color'] = item.selected? selectedColor:'#f5f5f5'
+					styles['background-color'] = item.selected?selectedColor:'#f5f5f5'
 				}
 				let classles = ''
 				for (let i in styles) {
